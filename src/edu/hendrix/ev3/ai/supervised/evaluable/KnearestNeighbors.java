c095ec632e6205ee6e6ebceded31e5575c8cbf5c
E-R-C <ehuynh.school@gmail.com>
2016-05-18 12:41:56 -0500

    K Nearest Neighbors Rewrite with K values
    Incorporated K and cleaned up imports

--- src/edu/hendrix/ev3/ai/supervised/evaluable/KnearestNeighbors.java
+++ src/edu/hendrix/ev3/ai/supervised/evaluable/KnearestNeighbors.java
@@ -2,10 +2,8 @@ package edu.hendrix.ev3.ai.supervised.evaluable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
-
 
 import edu.hendrix.ev3.ai.cluster.yuv.AdaptedYUYVImage;
 import edu.hendrix.ev3.ai.supervised.RobotLearner;
@@ -36,25 +34,48 @@ public class KnearestNeighbors implements RobotLearner {
 	@Override
 	public Move bestMatchFor(AdaptedYUYVImage img) {
 		long minDist = Long.MAX_VALUE;
-		AdaptedYUYVImage minImage = null;
-		Move minMove = Move.STOP;
+		TreeMap<Long, Move> kSet = new TreeMap<>();
 		for (Move m: neighbors.keySet()){
 			for (AdaptedYUYVImage i: neighbors.get(m)){
 				long tempDist = distFunction(i,img);
-				if (tempDist < minDist){
-					minDist = tempDist;
-					minImage = i;
-					minMove = m;
+				if (kSet.size() < k){
+					kSet.put(tempDist, m);
+				}
+				else if (tempDist < minDist){
+					kSet.put(tempDist, m);
+					kSet.pollLastEntry();
+					minDist = kSet.lastKey();
 				}
 			}
 		}
-		return minMove;
+		
+		return getMostCommonElement(kSet);
 		
 	}
 	public long distFunction(AdaptedYUYVImage img1, AdaptedYUYVImage img2){
 		// Function subject to change
 		long dist = YUYVDistanceFuncs.euclideanAllChannels(img1, img2);
 		return dist;
+	}
+	public Move getMostCommonElement(TreeMap<Long, Move> kClosest){
+		HashMap<Move,Integer> occurences = new HashMap<>();
+		for (Move m: kClosest.values()){
+			if (occurences.containsKey(m)){
+				occurences.put(m, occurences.get(m)+1);
+			}
+			else{
+				occurences.put(m, 1);
+			}
+		}
+		int maxValue = 0;
+		Move maxMove = Move.NONE;
+		for (Entry<Move, Integer> m1: occurences.entrySet()){
+			if (maxValue < m1.getValue()){
+				maxValue = m1.getValue();
+				maxMove = m1.getKey();
+			}
+		}
+		return maxMove;
 	}
 	@Override
 	public boolean isTrained() {
